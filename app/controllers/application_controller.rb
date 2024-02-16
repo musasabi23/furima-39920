520b212fe679b1d25058ab02d366eb1a6161bf5a
musasabi23 <oniokooniono0209@gmail.com>
2024-02-16 21:30:44 +0900

    バリデーション改善

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < ActionController::Base
     end
 
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
 
   end
 end
