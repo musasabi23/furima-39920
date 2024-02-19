e6b9eba8f961fbd2c1014908687e71f6c05c5b3b
musasabi23 <oniokooniono0209@gmail.com>
2024-02-19 16:43:58 +0900

    ActiveHash発送元の地域 実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :quality
   belongs_to :delivery_cost
+  belongs_to :prefecture
 
 
   with_options presence: true do
@@ -13,7 +14,8 @@ class Item < ApplicationRecord
 
     validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :quality_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :delivery_cost, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :delivery_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   end
 
