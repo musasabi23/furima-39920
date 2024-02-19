b17e6b8c9efeefb57b992be9874e801f4fefdbf3
musasabi23 <oniokooniono0209@gmail.com>
2024-02-19 15:11:03 +0900

    商品の画像、名、説明設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:name, :description, :category_id, :quality_id, :delivery_cost_id, :prefecture_id, :shipping_duration_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
