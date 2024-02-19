569d7b6225d4d9b33dc9bd1ba8d8f4ce02f9296f
musasabi23 <oniokooniono0209@gmail.com>
2024-02-19 16:20:20 +0900

    ActiveHash商品の状態 実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,17 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :quality
 
 
   with_options presence: true do
     validates :image
     validates :name
     validates :description
+
     validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :quality_id, numericality: { other_than: 1 , message: "can't be blank"}
+    
   end
 
 
