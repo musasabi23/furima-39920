88adc9b4ca0b867cbbe3f06da6862a6cda51b2fb
musasabi23 <oniokooniono0209@gmail.com>
2024-02-19 14:34:55 +0900

    商品出品ページ設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,6 @@ class ApplicationController < ActionController::Base
       params[:user][:birthday] = birthday
     end
 
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
 end
