fd9f9f6242b86b83af89de4f84687bc32a5546d9
musasabi23 <oniokooniono0209@gmail.com>
2024-02-20 16:53:48 +0900

    販売手数料と販売利益表示　実装

--- app/javascript/calculateProfit.js
+++ app/javascript/calculateProfit.js
@@ -1,5 +1,23 @@
-function post (){
-  console.log("非同期投稿を実装");
-};
+window.addEventListener('load', function(){
 
-window.addEventListener('turbo:load', post);
+  const price = document.getElementById("item-price")
+
+  price.addEventListener("input", () => {
+    const inputPrice = price.value;
+
+    const handlingFee = inputPrice * 0.1;
+    var handlingfee = Math.floor(handlingFee);
+
+    const profitCalculation = inputPrice * 0.9;
+    var proFit = Math.floor(profitCalculation);
+
+
+    const cost = document.getElementById("add-tax-price");
+    cost.innerHTML = handlingfee;
+
+    const profit = document.getElementById('profit');
+    profit.innerHTML = proFit;
+
+  })
+
+});
