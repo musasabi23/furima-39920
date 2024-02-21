2df199431ab51aee91a3be57bffab55d3ebac64d
musasabi23 <oniokooniono0209@gmail.com>
2024-02-21 14:04:10 +0900

    ルボコップ修正

--- app/javascript/calculateProfit.js
+++ app/javascript/calculateProfit.js
@@ -1,28 +1,23 @@
-function profitAndLoss() {
-
-//window.addEventListener('load', function(){
+document.addEventListener('turbo:load', function() {
 
   const price = document.getElementById("item-price")
+  if (price) {
 
-  price.addEventListener("input", () => {
-    const inputPrice = price.value;
+    price.addEventListener("input", () => {
+      const inputPrice = price.value;
 
-    const handlingFee = inputPrice * 0.1;
-    var handlingfee = Math.floor(handlingFee);
+      const handlingFee = inputPrice * 0.1;
+      const handlingfee = Math.floor(handlingFee);
 
-    const profitCalculation = inputPrice * 0.9;
-    var proFit = Math.floor(profitCalculation);
+      const profitCalculation = inputPrice * 0.9;
+      const proFit = Math.floor(profitCalculation);
 
 
-    const cost = document.getElementById("add-tax-price");
-    cost.innerHTML = handlingfee;
+      const cost = document.getElementById("add-tax-price");
+      cost.innerHTML = handlingfee;
 
-    const profit = document.getElementById('profit');
-    profit.innerHTML = proFit;
-
-  })
-
-//});
-}
-
-window.addEventListener('turbo:load', profitAndLoss);
+      const profit = document.getElementById('profit');
+      profit.innerHTML = proFit;
+    });
+  };
+});
