3493c35cf09f77edc4879920769abaa2daf299d2
musasabi23 <oniokooniono0209@gmail.com>
2024-02-23 07:44:41 +0900

    データ保存確認

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,11 +3,5 @@ class Customer < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
-  #with_options presence: true do
-    #validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
-    #validates :city
-    #validates :street_address
-    #validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-  #end
-  #validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+ 
 end
--- app/models/purchase_customer.rb
+++ app/models/purchase_customer.rb
@@ -1,6 +1,6 @@
 class PurchaseCustomer
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture, :city, :street_address, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :post_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :item_id, :user_id
 
   with_options presence: true do
     validates :user_id
@@ -10,11 +10,11 @@ class PurchaseCustomer
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
-  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
-    purchase = Purchase.create(item_id: params[:item_id], user_id: current_user.id)
-    Customer.create(post_code: post_code, prefecture: prefecture, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    Customer.create(post_code: post_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
     
 end
