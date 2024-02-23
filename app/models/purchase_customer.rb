a6547e234ef2dd830c3116834bd399d47a7560ce
musasabi23 <oniokooniono0209@gmail.com>
2024-02-23 10:55:02 +0900

    テストコード追加

--- app/models/purchase_customer.rb
+++ app/models/purchase_customer.rb
@@ -8,7 +8,7 @@ class PurchaseCustomer
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :city
     validates :street_address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
   end
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
--- spec/models/purchase_customer_spec.rb
+++ spec/models/purchase_customer_spec.rb
@@ -24,6 +24,50 @@ RSpec.describe PurchaseCustomer, type: :model do
         expect(@purchase_customer.errors.full_messages).to include("Post code can't be blank")
       end
 
+      it 'post_codeが3桁-4桁でなければ購入できない' do
+        @purchase_customer.post_code = '12-34567'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = '1234-567'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = '124-567'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = '1234-5678'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+      end
+
+      it 'post_codeが半角数字でなければ購入できない' do
+        @purchase_customer.post_code = '１２３-４５６７'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = 'asd-fghj'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = 'あいう-えおかき'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = 'アイウ-エオカキ'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = 'ｱｲｳ-ｴｵｶｷ'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+
+        @purchase_customer.post_code = '亜亜亜-亜亜亜亜'
+        @purchase_customer.valid?
+        expect(@purchase_customer.errors.full_messages).to include("Post code is invalid")
+      end
+
       it 'prefecture_idを選択しなければ購入できない' do
         @purchase_customer.prefecture_id = '0'
         @purchase_customer.valid?
